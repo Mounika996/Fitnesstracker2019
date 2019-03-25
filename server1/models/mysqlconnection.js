7116247d968c6be7618ac11dac486ac59688bb5b
Mounika <vinnymounika96@gmail.com>
2019-03-24 23:55:56 -0400

    save changes

--- server1/models/mysqlconnection.js
+++ server1/models/mysqlconnection.js
@@ -1,13 +1,13 @@
-const mysql =   require('mysql');
-                require('dotenv').config('./models/.env');
+//const mysql =   require('mysql');
+  //              require('dotenv').config('./models/.env');
 
-const conn = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DB,
-    connectionLimit: 10,
-    insecureAuth: true
-});
+//const conn = mysql.createPool({
+  //  host: process.env.MYSQL_HOST,
+    //user: process.env.MYSQL_USER,
+   // password: process.env.MYSQL_PASS,
+    //database: process.env.MYSQL_DB,
+    //connectionLimit: 10,
+    //insecureAuth: true
+//});
 
-module.exports = conn;
+// module.exports = conn;
