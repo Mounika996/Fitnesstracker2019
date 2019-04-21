955890db48a63e55b9e0ebe0ed2bc8eff935d86b
mounika996 <vinnymounika96@gmsil.com>
2019-04-20 22:30:32 -0400

    front end changes

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<router-view/>
+</template>
+
+<script>
+	
+	export default {
+		name: 'App'
+	}
+
+</script>
+
+<style>
+	@import './assets/vendor/bootstrap/css/bootstrap.min.css';
+	@import './assets/vendor/fontawesome/css/font-awesome.min.css';
+
+	@import './assets/css/styles.css';
+</style>
